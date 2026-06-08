8051ade3d27153c86a0b3f1779361e8a94025824
lvs <lakshya9vijay@gmail.com>
2026-06-08 18:58:34 +0530

    Add star pyramid pattern program

---
+++ Day_10/star_pyramid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, i, j, k;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Print star pyramid
+    for(i = 1; i <= rows; i++){
+        // Print spaces
+        for(j = 1; j <= rows - i; j++){
+            printf(" ");
+        }
+
+        // Print stars
+        for(k = 1; k <= (2 * i - 1); k++){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
