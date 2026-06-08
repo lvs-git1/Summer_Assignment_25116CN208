9c9897e9f2cedabfddf664093c76aff469abc853
lvs <lakshya9vijay@gmail.com>
2026-06-08 18:40:56 +0530

    Add half pyramid pattern program

---
+++ Day_8/half_pyramid_pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int rows, i, j;
+
+    // Take input from the user
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Print half pyramid
+    for(i = 1; i <= rows; i++){
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
