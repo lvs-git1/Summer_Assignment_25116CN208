47c3ab95b281beebfa3a6e5e8d45a91bfc4d2f13
lvs <lakshya9vijay@gmail.com>
2026-06-08 18:55:33 +0530

    Add repeated character pattern program

---
+++ Day_9/repeated_character_pattern.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Print pattern
+    for(i = 1; i <= rows; i++){
+        for(j = 1; j <= i; j++){
+            printf("%c", 'A' + i - 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
