7da45c3326694cd0b72032cad92e933f07d47db7
lvs <lakshya9vijay@gmail.com>
2026-06-08 18:53:32 +0530

    Add reverse number triangle pattern program

---
+++ Day_9/reverse_number_triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Print reverse number triangle
+    for(i = rows; i >= 1; i--){
+        for(j = 1; j <= i; j++){
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
