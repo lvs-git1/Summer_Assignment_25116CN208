8177ef3566a577df04079a7676bf896c5cb6467a
lvs <lakshya9vijay@gmail.com>
2026-06-17 21:42:24 +0530

    Add intersection of arrays program

---
+++ Day_17/intersection_of_arrays.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr1[100], arr2[100];
+    int n1, n2, i, j, k;
+    int found;
+
+    printf("Enter the number of elements in first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter %d elements:\n", n1);
+    for(i = 0; i < n1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter the number of elements in second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter %d elements:\n", n2);
+    for(i = 0; i < n2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    printf("Intersection of arrays:\n");
+
+    for(i = 0; i < n1; i++)
+    {
+        found = 0;
+
+        for(k = 0; k < i; k++)
+        {
+            if(arr1[i] == arr1[k])
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if(found)
+        {
+            continue;
+        }
+
+        for(j = 0; j < n2; j++)
+        {
+            if(arr1[i] == arr2[j])
+            {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
